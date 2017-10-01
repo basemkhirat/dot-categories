f4c2014627ea6233e3d25e395d52dd04cd901823
basemkhirat <basemkhirat@gmail.com>
2017-10-01 16:13:20 +0200

    remove_info_method

--- src/Plugin.php
+++ src/Plugin.php
@@ -13,25 +13,11 @@ class Plugin extends \Dot\Platform\Plugin
         "manage"
     ];
 
-    /**
-     * @return array
-     */
-    function info()
-    {
-
-        return [
-            "name" => "categories",
-            "version" => "1.0",
-        ];
-
-    }
-
     function boot()
     {
         Navigation::menu("sidebar", function ($menu) {
 
             if (Gate::allows("categories.manage")) {
-                //$menu->item('news_options.categories', trans("categories::categories.categories"), URL::to(ADMIN . '/categories'))->icon("fa-folder")->order(1);
                 $menu->item('categories', trans("categories::categories.categories"), URL::to(ADMIN . '/categories'))->icon("fa-folder")->order(1);
             }
         });
