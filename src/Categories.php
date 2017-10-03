9cdbfade8e0e9b00cc5ebe862bbc0435ebac75ba
basemkhirat <basemkhirat@gmail.com>
2017-10-03 08:26:32 +0200

    extend

--- src/Categories.php
+++ src/Categories.php
@@ -6,22 +6,23 @@ use Gate;
 use Navigation;
 use URL;
 
-class Plugin extends \Dot\Platform\Plugin
+class Categories extends \Dot\Platform\Plugin
 {
 
-    public $permissions = [
+    protected $permissions = [
         "manage"
     ];
 
     function boot()
     {
+
+        parent::boot();
+
         Navigation::menu("sidebar", function ($menu) {
 
             if (Gate::allows("categories.manage")) {
                 $menu->item('categories', trans("categories::categories.categories"), URL::to(ADMIN . '/categories'))->icon("fa-folder")->order(1);
             }
         });
-
-        include __DIR__ . "/routes.php";
     }
 }
